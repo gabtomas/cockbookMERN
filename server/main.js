484692bc1d5bc53ce1ae20e5b90322ea38adb14b
gabtomas <46448735+gabtomas@users.noreply.github.com>
2023-09-12 17:28:27 +0200

    react installation

--- server/main.js
+++ server/main.js
@@ -4,12 +4,18 @@ const port = 3000;
 
 //import ingredient controller
 const ingredientController = require("./controllers/ingredient.js");
+const recipeController = require("./controllers/recipe.js");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+
 //use ingredient controller
 app.use("/ingredient", ingredientController);
+app.use("/recipe", recipeController);
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
